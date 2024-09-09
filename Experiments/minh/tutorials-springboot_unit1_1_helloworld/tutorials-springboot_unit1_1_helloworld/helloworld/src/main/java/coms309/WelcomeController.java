b9cda7b5a62683915f447e589729d06fa89e5b3f
Minh Nguyen <tamminh@iastate.edu>
2024-09-09 14:27:28 -0500

    Updating unit 1 backend

--- Experiments/minh/tutorials-springboot_unit1_1_helloworld/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/minh/tutorials-springboot_unit1_1_helloworld/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/WelcomeController.java
@@ -4,12 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController // GET, POST, PUT, DELETE
+    // Postmapping
 class WelcomeController {
 
-    @GetMapping("/")
+    @GetMapping("/") // Handle GET request
     public String welcome() {
         return "Hello and welcome to COMS 309";
+    }
+
+    @GetMapping("/demo1/springboot")
+    public Map<String, String> demo1() {
+        Map<String, String> response = new HashMap<>();
+        response.put("date", "2024-09-10");
+        response.put("purpose", "demo 01");
+        return response;
     }
 
     @GetMapping("/{name}")
