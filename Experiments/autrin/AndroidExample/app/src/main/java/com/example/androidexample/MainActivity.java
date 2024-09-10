5b66789839ed721969742fe37856299250a2d0fc
Autrin <autrinhakimi@gmail.com>
2024-09-10 13:13:24 -0500

    Added a text clock and set its format to 12hr and size to 30.

--- Experiments/autrin/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/autrin/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextClock;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -13,7 +14,7 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
-
+    private TextClock textClock;    // define textclock variable
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
         messageText.setText("Today");
         messageText.setTextColor(getResources().getColor(R.color.red));
         messageText.setTextSize(30);
+
+        textClock = findViewById(R.id.main_text_clock);     // link to textclock in the Main activity XML
+        textClock.setFormat12Hour("hh:mm:ss a");
+        textClock.setTextSize(30);
     }
 }
