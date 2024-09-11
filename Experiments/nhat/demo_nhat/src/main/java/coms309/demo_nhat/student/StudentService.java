73c619fecc717168a3b129e2aaa89ef6a809b80a
Nhat Le <anhat.ptnk2023@gmail.com>
2024-09-11 14:24:04 -0500

    Add PUT and DELETE operations

--- Experiments/nhat/demo_nhat/src/main/java/coms309/demo_nhat/student/StudentService.java
+++ Experiments/nhat/demo_nhat/src/main/java/coms309/demo_nhat/student/StudentService.java
@@ -1,5 +1,6 @@
 package coms309.demo_nhat.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service // Related to Business Logic
@@ -33,5 +35,35 @@ public class StudentService {
         if (studentOptional.isPresent())
             throw new IllegalStateException("Email taken");
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exist");
+        }
+
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional // Indicate Spring Boot to manage transaction for the entire method. An entity returned within a transactional context is managed.
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id " + studentId + " does not exist"));
+
+
+        if (name != null && name.length() > 0 &&
+                !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> emailExist = studentRepository.findStudentByEmail(email);
+            if (emailExist.isPresent())
+                throw new IllegalStateException("Email is taken");
+            student.setEmail(name);
+        }
     }
 }
