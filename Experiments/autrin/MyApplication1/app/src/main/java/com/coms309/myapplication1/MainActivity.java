e66f48102d54a54bc8fef3eada553940208f5b3a
Autrin <autrinhakimi@gmail.com>
2024-09-28 21:28:19 -0500

    Added a post class

--- Experiments/autrin/MyApplication1/app/src/main/java/com/coms309/myapplication1/MainActivity.java
+++ Experiments/autrin/MyApplication1/app/src/main/java/com/coms309/myapplication1/MainActivity.java
@@ -2,7 +2,7 @@ package com.coms309.myapplication1;
 
 import android.os.Bundle;
 import android.widget.TextView;
-
+import com.google.gson.annotations.SerializedName;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -21,9 +21,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        TextView apiText1 = findViewById(R.id.activity_main_textView1);
+        TextView apiText1 = findViewById(R.id.activity_main_textView1); //reference to the textview
 
         // to communicate with the server
     }
 }
 
+class Post{
+    private int userId;
+    private int id;
+    private String title;
+    @SerializedName("body")
+    private String bigText;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBigText() {
+        return bigText;
+    }
+
+    public void setBigText(String bigText) {
+        this.bigText = bigText;
+    }
+}
