93217f5f82e9b496b0eebbca52304660cbf78c5b
Autrin <autrinhakimi@gmail.com>
2024-10-01 19:47:21 -0500

    Moved the api code to its own class and create an instance of it. check the api error to be not null and then log it. Handle the response of the api and update the UI. Notify the adapter that the data has changed.

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/home/HomeFragment.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/home/HomeFragment.java
@@ -14,23 +14,12 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.coms309.isu_pulse_frontend.api.ApiService;
 import com.coms309.isu_pulse_frontend.databinding.FragmentHomeBinding;
 
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.StringRequest;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 
 public class HomeFragment extends Fragment {
 
@@ -42,7 +31,7 @@ public class HomeFragment extends Fragment {
     private List<String> events = new ArrayList<>();
     private List<String> announcements = new ArrayList<>();
 
-    public final String URL_STRING_REQ = "http://10.0.2.2:8080/tasksduetoday";
+    private ApiService apiService;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -72,6 +61,8 @@ public class HomeFragment extends Fragment {
         List<String> days = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
         WeeklyCalendarAdapter adapter = new WeeklyCalendarAdapter(days, tasksDueToday, events);
         recyclerView.setAdapter(adapter);
+
+        apiService = new ApiService(getContext());
         populateTasksDueToday();
 
         return root;
@@ -83,38 +74,22 @@ public class HomeFragment extends Fragment {
         binding = null;
     }
 
-    public void populateTasksDueToday(){
-        // First roundtrip to the server to get the tasks due today
-         getTasksDueToday();
-    }
-    public void getTasksDueToday(){ // might need to change this to JsonArrayRequest if we want to get a list of tasks
-        JsonArrayRequest jsonArrReq = new JsonArrayRequest(Request.Method.GET, URL_STRING_REQ, null, new Response.Listener<JSONArray>() {
+    public void populateTasksDueToday() {
+        apiService.getTasksDueToday(new ApiService.TaskResponseListener() {
             @Override
-            public void onResponse(JSONArray response) {
-                Log.d("Response: " , response.toString());
-                for (int i = 0; i < response.length(); i++) {
-                    try {
-                        JSONObject jsonObject = response.getJSONObject(i);
-                        // TODO - get the properties of the task object
-                        Long cId = jsonObject.getLong("cId");
-                        Long tId = jsonObject.getLong("tId");
-                        String section = jsonObject.getString("section");
-                        String title = jsonObject.getString("title");
-                        String description = jsonObject.getString("description");
-                        Date date = Date.valueOf(jsonObject.getString("date"));
-                        String taskType = jsonObject.getString("taskType");
-
-//                        tasksDueToday.add(task);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+            public void onResponse(List<ListTaskObject> tasks) {
+                // Handle the response and update the UI
+                for (ListTaskObject task : tasks) {
+                    tasksDueToday.add(task.getTitle());
                 }
+                // Notify the adapter that the data has changed
+                binding.recyclerViewWeeklyCalendar.getAdapter().notifyDataSetChanged();
             }
 
-        }, new Response.ErrorListener(){
             @Override
-            public void onErrorResponse(VolleyError error){
-                VolleyLog.e("Error: " + error.getMessage());
+            public void onError(String message) {
+                String errorMessage = message != null ? message : "Unknown error";
+                Log.e("API Error", errorMessage);
             }
         });
     }
