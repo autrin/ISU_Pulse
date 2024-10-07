b7bb279b6a47473a60f2aa017fcefd4e8ce9119a
Autrin <autrinhakimi@gmail.com>
2024-10-07 00:07:45 -0500

    updated the get method to get all the properties of a task

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/api/ApiService.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/api/ApiService.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class ApiService {
 
     private static final String URL_STRING_REQ = "https://ae21ce63-e030-423f-aa6f-a80597a900cf.mock.pstmn.io";
-    private static final String URL_STRING_REQ_VM = "http://coms-3090-042.class.las.iastate.edu:8080/task/getTaskByUserIn2days";
+    private static final String URL_STRING_REQ_VM = "http://coms-3090-042.class.las.iastate.edu:8080/task/getTaskByUserIn2days/n001";
     private Context context;
 
     public ApiService(Context context) {
@@ -35,7 +35,7 @@ public class ApiService {
 
     public void getTasksDueToday(TaskResponseListener listener) {
         String url = URL_STRING_REQ + "/tasksduetoday";
-        JsonArrayRequest jsonArrReq = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+        JsonArrayRequest jsonArrReq = new JsonArrayRequest(Request.Method.GET, URL_STRING_REQ_VM, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
                 Log.d("Response: ", response.toString());
@@ -43,15 +43,25 @@ public class ApiService {
                 for (int i = 0; i < response.length(); i++) {
                     try {
                         JSONObject jsonObject = response.getJSONObject(i);
+
+                        // Extracting task-related properties
                         Long cId = jsonObject.getLong("cId");
                         Long tId = jsonObject.getLong("tId");
                         String section = jsonObject.getString("section");
                         String title = jsonObject.getString("title");
                         String description = jsonObject.getString("description");
-                        Date date = Date.valueOf(jsonObject.getString("date"));
+                        Date dueDate = Date.valueOf(jsonObject.getString("dueDate"));
                         String taskType = jsonObject.getString("taskType");
 
-                        ListTaskObject task = new ListTaskObject(cId, tId, section, title, description, date, taskType);
+                        // Extracting additional fields
+                        String courseName = jsonObject.getString("courseName");
+                        int credits = jsonObject.getInt("credits");
+                        int mediumTerm = jsonObject.getInt("mediumTerm");
+                        String roomName = jsonObject.getString("roomName");
+                        String building = jsonObject.getString("building");
+
+                        // Creating the task object with updated fields
+                        ListTaskObject task = new ListTaskObject(cId, tId, section, title, description, dueDate, taskType, courseName, credits, mediumTerm, roomName, building);
                         tasks.add(task);
                     } catch (Exception e) {
                         e.printStackTrace();
