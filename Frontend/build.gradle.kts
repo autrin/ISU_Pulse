e196398f1300108baae384cf9946aa91cce15b72
Bach Nguyen <nguyentatbach605@gmail.com>
2024-10-10 20:03:49 -0500

    Update gradle

--- Frontend/build.gradle.kts
+++ Frontend/build.gradle.kts
@@ -7,4 +7,5 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
