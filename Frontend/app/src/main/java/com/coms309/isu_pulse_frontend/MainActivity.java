0892d9cdf79551ff5aeda20791b26e1dc6bccb84
Bach Nguyen <nguyentatbach605@gmail.com>
2024-10-10 20:04:55 -0500

    Update Main Activity

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/MainActivity.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
+//        setContentView(R.layout.profile);
 
         signInButton = findViewById(R.id.signInButton);
         signUpButton = findViewById(R.id.signUpButton);
