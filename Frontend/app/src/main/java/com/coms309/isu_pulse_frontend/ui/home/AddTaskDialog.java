a0a4e3ab074cb3ba534dfeff08f40bf209d6cb83
Autrin <autrinhakimi@gmail.com>
2024-10-11 12:02:00 -0500

    convert duedatastring to a unix timestamp

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/home/AddTaskDialog.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/home/AddTaskDialog.java
@@ -6,12 +6,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
 import com.coms309.isu_pulse_frontend.R;
 import com.coms309.isu_pulse_frontend.adapters.TaskListAdapter;
 import com.coms309.isu_pulse_frontend.api.TaskApiService;
+import com.coms309.isu_pulse_frontend.ui.home.HomeFragment;
+import com.coms309.isu_pulse_frontend.ui.home.PersonalTask;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class AddTaskDialog extends DialogFragment {
 
@@ -41,8 +48,14 @@ public class AddTaskDialog extends DialogFragment {
                 String title = editTextTitle.getText().toString();
                 String description = editTextDescription.getText().toString();
                 String dueDateString = editTextDueDate.getText().toString();
+                long dueDateTimestamp = 0;
+                try {
+                    dueDateTimestamp = new SimpleDateFormat("yyyy-MM-dd").parse(dueDateString).getTime() / 1000;
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
 
-                PersonalTask newTask = new PersonalTask(title, description, dueDateString, "n001");
+                PersonalTask newTask = new PersonalTask(title, description, String.valueOf(dueDateTimestamp), "n001");
                 taskApiService.createPersonalTask(newTask);
                 homeFragment.addNewTask(newTask);
                 dismiss();
