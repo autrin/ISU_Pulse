feedc00bd1e87a06174fbfd14a5c7a7149eded05
Bach Nguyen <nguyentatbach605@gmail.com>
2024-10-10 20:04:15 -0500

    Update controller

--- Backend/demo/src/main/java/coms309/backEnd/demo/controller/UserController.java
+++ Backend/demo/src/main/java/coms309/backEnd/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package coms309.backEnd.demo.controller;
 import coms309.backEnd.demo.entity.User;
 import coms309.backEnd.demo.entity.UserType;
 import coms309.backEnd.demo.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -50,5 +51,22 @@ public class UserController {
         response.put("message", "Successfully registered new user.");
         return ResponseEntity.ok(response);
     }
+
+    @Transactional
+    @PutMapping(path = "updatepw/{netId}")
+    public ResponseEntity<String> updateUserAccount(@PathVariable String netId, @RequestParam(required = true) String newPassword) {
+        Optional<User> userOptional = userRepository.findById(netId);
+        if (!userOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User does not exist");
+        }
+
+        User user = userOptional.get();
+        if (user.getHashedPassword().equals(newPassword))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("New password must be different from the old password");
+
+        user.setHashedPassword(newPassword);
+        return ResponseEntity.status(HttpStatus.OK).body("Password updated successfully");
+    }
+
 
 }
