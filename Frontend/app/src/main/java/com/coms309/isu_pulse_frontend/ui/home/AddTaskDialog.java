e1ff9db1a71f5c9b3d605d2a40ee1da6183f81cc
Autrin <autrinhakimi@gmail.com>
2024-10-10 20:51:36 -0500

    created the dialog class

---
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/home/AddTaskDialog.java
@@ -0,0 +1,14 @@
+package com.coms309.isu_pulse_frontend.ui.home;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+public class AddTaskDialog extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
