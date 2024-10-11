0ea6cbc21a21bffcb99b5607feb8c75cc0e8113b
Nhat Le <anhat.ptnk2023@gmail.com>
2024-10-11 08:04:46 -0500

    add JsonIgnore for profile

--- Backend/demo/src/main/java/coms309/backEnd/demo/entity/User.java
+++ Backend/demo/src/main/java/coms309/backEnd/demo/entity/User.java
@@ -1,5 +1,6 @@
 package coms309.backEnd.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -25,6 +26,7 @@ public class User {
     private Set<Enroll> enrollments;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIgnore
     private Profile profile;
 
     // Constructors
