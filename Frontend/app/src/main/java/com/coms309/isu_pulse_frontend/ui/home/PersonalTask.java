95181a332e80a67719792b31b16122834a78225f
Autrin <autrinhakimi@gmail.com>
2024-10-11 11:02:08 -0500

    added the personal task getters and setters

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/home/PersonalTask.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/home/PersonalTask.java
@@ -12,4 +12,36 @@ public class PersonalTask {
         this.dueDate = dueDate;
         this.userNetId = userNetId;
     }
+    public String getUserNetId() {
+        return userNetId;
+    }
+
+    public void setUserNetId(String userNetId) {
+        this.userNetId = userNetId;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
