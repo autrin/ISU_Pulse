4272d4bfe068a4ce7d92cbe176571b0052bf5276
Autrin <autrinhakimi@gmail.com>
2024-10-12 00:34:53 -0500

    created a profile class along with its setters and getters

---
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/model/Profile.java
@@ -0,0 +1,84 @@
+// Profile.java
+package com.coms309.isu_pulse_frontend.model;
+
+public class Profile {
+    private String profilePictureUrl;
+    private String firstName;
+    private String lastName;
+    private ProfileDetails profile;
+
+    public ProfileDetails getProfile() {
+        return profile;
+    }
+
+    public void setProfile(ProfileDetails profile) {
+        this.profile = profile;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
+
+
+
+
+    public static class ProfileDetails {
+        private int id;
+        private String linkedinUrl;
+        private String externalUrl;
+        private String description;
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getExternalUrl() {
+            return externalUrl;
+        }
+
+        public void setExternalUrl(String externalUrl) {
+            this.externalUrl = externalUrl;
+        }
+
+        public String getLinkedinUrl() {
+            return linkedinUrl;
+        }
+
+        public void setLinkedinUrl(String linkedinUrl) {
+            this.linkedinUrl = linkedinUrl;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+    }
+}
