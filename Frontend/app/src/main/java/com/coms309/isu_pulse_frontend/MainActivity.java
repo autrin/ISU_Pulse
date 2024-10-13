7db77a93d04e4570790e24c33cad8d517642010a
Autrin <autrinhakimi@gmail.com>
2024-10-12 20:11:53 -0500

    added another url

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/MainActivity.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+//
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -67,8 +67,8 @@ public class MainActivity extends AppCompatActivity {
         });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+//         Passing each menu ID as a set of Ids because each
+//         menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
