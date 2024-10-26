0401ce1f7c1ec5b8245d8209c681b3e98073248f
Autrin <autrinhakimi@gmail.com>
2024-10-25 19:37:49 -0500

    Added support for message format consistency and status updates.

--- Experiments/autrin/unit3/tutorials-unit3_1_websockets/WebSocketAndorid-service-approach/app/src/main/java/com/example/androidexample/ChatActivity2.java
+++ Experiments/autrin/unit3/tutorials-unit3_1_websockets/WebSocketAndorid-service-approach/app/src/main/java/com/example/androidexample/ChatActivity2.java
@@ -12,27 +12,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class ChatActivity2 extends AppCompatActivity {
 
     private Button sendBtn;
     private EditText msgEtx;
-    private TextView msgTv;
+    private TextView msgTv, statusTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat2);
 
-        /* initialize UI elements */
-        sendBtn = (Button) findViewById(R.id.sendBtn2);
-        msgEtx = (EditText) findViewById(R.id.msgEdt2);
-        msgTv = (TextView) findViewById(R.id.tx2);
+        sendBtn = findViewById(R.id.sendBtn2);
+        msgEtx = findViewById(R.id.msgEdt2);
+        msgTv = findViewById(R.id.tx2);
+        statusTv = findViewById(R.id.statusTextView); // Add TextView for status
 
-        /* send button listener */
         sendBtn.setOnClickListener(v -> {
-            // broadcast this message to the WebSocketService
-            // tag it with the key - to specify which WebSocketClient (connection) to send
-            // in this case: "chat2"
             Intent intent = new Intent("SendWebSocketMessage");
             intent.putExtra("key", "chat2");
             intent.putExtra("message", msgEtx.getText().toString());
@@ -40,17 +40,15 @@ public class ChatActivity2 extends AppCompatActivity {
         });
     }
 
-    // For receiving messages
-    // only react to messages with tag "chat2"
     private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String key = intent.getStringExtra("key");
             if ("chat2".equals(key)){
                 String message = intent.getStringExtra("message");
+                String timestamp = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
                 runOnUiThread(() -> {
-                    String s = msgTv.getText().toString();
-                    msgTv.setText(s + "\n" + message);
+                    msgTv.append("\n" + timestamp + ": " + message);
                 });
             }
         }
