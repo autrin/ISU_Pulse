cdc31f6e272e9374378f32327db95574ba78534a
Nhat Le <anhat.ptnk2023@gmail.com>
2024-11-01 23:21:57 -0500

    delete redundant spacing

--- Backend/demo/src/main/java/coms309/backEnd/demo/repository/ScheduleRepository.java
+++ Backend/demo/src/main/java/coms309/backEnd/demo/repository/ScheduleRepository.java
@@ -2,7 +2,10 @@ package coms309.backEnd.demo.repository;
 
 import coms309.backEnd.demo.entity.Profile;
 import coms309.backEnd.demo.entity.Schedule;
+import coms309.backEnd.demo.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
