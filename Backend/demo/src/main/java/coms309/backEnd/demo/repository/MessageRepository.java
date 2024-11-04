baaf9832be79067af72fac40bd86b3f270b86e65
Tat Bach Nguyen <ntbach@iastate.edu>
2024-11-04 18:25:32 +0000

    Revert "Merge branch 'displaying_friendList' into 'main'"
    This reverts merge request !49

--- Backend/demo/src/main/java/coms309/backEnd/demo/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package coms309.backEnd.demo.repository;
-
-import coms309.backEnd.demo.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message,Long> {
-}
