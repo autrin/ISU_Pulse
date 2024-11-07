c1dcfcd85fabb8798a94e5ac6efbf83da97867e3
Autrin <autrinhakimi@gmail.com>
2024-11-07 16:22:06 -0600

    update course import

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/course_functional/CourseView.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/course_functional/CourseView.java
@@ -14,7 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.coms309.isu_pulse_frontend.R;
 import com.coms309.isu_pulse_frontend.api.CourseService;
 import com.coms309.isu_pulse_frontend.proifle_activity.ProfileActivity;
-import com.coms309.isu_pulse_frontend.ui.home.Course;
+import com.coms309.isu_pulse_frontend.model.Course;
 
 import java.util.ArrayList;
 import java.util.List;
