84e4908204f16c02b474a725e24d5c75c8403986
Autrin <autrinhakimi@gmail.com>
2024-11-07 20:59:29 -0600

    update imports due change in directory

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/loginsignup/SignupActivity.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/loginsignup/SignupActivity.java
@@ -14,7 +14,6 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,11 +22,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.coms309.isu_pulse_frontend.MainActivity;
 import com.coms309.isu_pulse_frontend.R;
 import com.coms309.isu_pulse_frontend.api.AuthenticationService;
-import com.coms309.isu_pulse_frontend.model.Profile;
-import com.coms309.isu_pulse_frontend.proifle_activity.ProfileActivity;
+import com.coms309.isu_pulse_frontend.ui.profile.ProfileActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
@@ -37,7 +34,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import org.json.JSONException;
 import org.json.JSONObject;
