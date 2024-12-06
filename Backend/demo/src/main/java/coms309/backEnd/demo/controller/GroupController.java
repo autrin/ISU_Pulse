673103371b157da5ba5860c9c84c1db4f2cbf64b
Minh Nguyen <tamminh@iastate.edu>
2024-12-06 00:28:44 -0600

    Updating group chatting feature including friendship controller, chatController, GroupController

--- Backend/demo/src/main/java/coms309/backEnd/demo/controller/GroupController.java
+++ Backend/demo/src/main/java/coms309/backEnd/demo/controller/GroupController.java
@@ -8,10 +8,7 @@ import coms309.backEnd.demo.repository.JoinRepository;
 import coms309.backEnd.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
--- Backend/demo/src/main/java/coms309/backEnd/demo/entity/GroupMessages.java
+++ Backend/demo/src/main/java/coms309/backEnd/demo/entity/GroupMessages.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Data
 @Entity
 @Table(name = "group_messages")
-public class GroupMessages {
+public class GroupMessages implements Message{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
