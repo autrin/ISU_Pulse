5556cedbf927bb1e96631b9a2b215021a36c78e4
Minh Nguyen <tamminh@iastate.edu>
2024-12-07 05:37:02 -0600

    Updating group features

--- Backend/demo/src/main/java/coms309/backEnd/demo/repository/UserRepository.java
+++ Backend/demo/src/main/java/coms309/backEnd/demo/repository/UserRepository.java
@@ -6,10 +6,8 @@ import coms309.backEnd.demo.entity.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
    // public List<Course> findCourseByNetId(User user);
     @Query("SELECT u FROM User u WHERE u.netId = ?1")
