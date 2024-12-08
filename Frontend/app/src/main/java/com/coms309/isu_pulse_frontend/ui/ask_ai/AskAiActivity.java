d77312887c8662b413950dafe6c5c751ddc01f0a
Autrin <autrinhakimi@gmail.com>
2024-12-08 14:28:24 -0600

    Added the elements of ask ai

---
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/ask_ai/AskAiActivity.java
@@ -0,0 +1,20 @@
+package com.coms309.isu_pulse_frontend.ui.ask_ai;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AskAiActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Button searchButton;
+    EditText searchBar;
+    Button addButton;
+    TextView title;
+    ImageButton backButton;
+
+
+}
