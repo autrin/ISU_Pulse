53df7dec3727fbf5bab12a7b0c2920bb7bd69bfe
Autrin <autrinhakimi@gmail.com>
2024-12-08 14:57:12 -0600

    Open ask ai activity when clicked on

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/MainActivity.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/MainActivity.java
@@ -13,6 +13,7 @@ import com.coms309.isu_pulse_frontend.loginsignup.SignupActivity;
 import com.coms309.isu_pulse_frontend.loginsignup.UserSession;
 import com.coms309.isu_pulse_frontend.profile_activity.ProfileActivity;
 import com.coms309.isu_pulse_frontend.student_display.DisplayStudent;
+import com.coms309.isu_pulse_frontend.ui.ask_ai.AskAiActivity;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -90,6 +91,10 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 } else if (id == R.id.nav_logout) {
                     startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    drawer.closeDrawers();
+                    return true;
+                } else if (id == R.id.nav_ask_ai) {
+                    startActivity(new Intent(MainActivity.this, AskAiActivity.class));
                     drawer.closeDrawers();
                     return true;
                 } else {
