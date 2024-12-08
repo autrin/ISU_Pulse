c36405ce837e8ff4d7d67cdde1d96057dd289a25
Autrin <autrinhakimi@gmail.com>
2024-12-08 14:31:20 -0600

    Assign the properties of the class to their xml id's

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/ask_ai/AskAiActivity.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/ask_ai/AskAiActivity.java
@@ -1,5 +1,6 @@
 package com.coms309.isu_pulse_frontend.ui.ask_ai;
 
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -8,13 +9,26 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.coms309.isu_pulse_frontend.R;
+
 public class AskAiActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     Button searchButton;
     EditText searchBar;
-    Button addButton;
+    ImageButton addButton;
     TextView title;
     ImageButton backButton;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_screen);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        searchButton = findViewById(R.id.search_button);
+        searchBar = findViewById(R.id.search_bar);
+        addButton = findViewById(R.id.add_button);
+        title = findViewById(R.id.title);
+        backButton = findViewById(R.id.back_button);
+    }
 }
