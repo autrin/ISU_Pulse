2255b1a45e770679d537f61b42da46f492391f8a
Autrin <autrinhakimi@gmail.com>
2024-12-10 10:10:35 -0600

    Added onCreate to the class and inflate the view with celendar and find the grid

--- Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/calendar/ClassCalendar.java
+++ Frontend/app/src/main/java/com/coms309/isu_pulse_frontend/ui/calendar/ClassCalendar.java
@@ -1,4 +1,27 @@
 package com.coms309.isu_pulse_frontend.ui.calendar;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+
+import androidx.annotation.NonNull;
+
+import com.coms309.isu_pulse_frontend.R;
+
 public class ClassCalendar {
+    GridLayout calendarGrid;
+
+    public View onCreate(@NonNull LayoutInflater inflater,
+                         ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.calendar, container, false);
+        calendarGrid = root.findViewById(R.id.calendar_grid);
+
+
+
+        return root;
+    }
+
+
 }
